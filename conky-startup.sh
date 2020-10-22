9dd275054ba55dbe05beb4819c8078d733185a70
davidq2010 <david.qin@richmond.edu>
2020-10-22 11:11:10 -0400

    Push conky startup commands to background.

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,2 +1,2 @@
-conky -c ~/.conky/DateTime/DateTime.conf --daemonize --pause=5
-conky -c ~/.conky/Sidebar/Sidebar.conf --daemonize --pause=5
+conky -c ~/.conky/DateTime/DateTime.conf --daemonize --pause=5 &
+conky -c ~/.conky/Sidebar/Sidebar.conf --daemonize --pause=5 &
